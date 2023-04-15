2634fff8b53f6c6758ae9cd5fd4078f8642945e2
James Cooper <james.ioioio55@gmail.com>
2023-04-15 10:52:06 -0600

    added a new page to view individual blogs, and turncoted the text in the main page

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express();
 
 // this is commonly used when accessing css files. 
 app.use(express.static('public'));
+app.use('/viewBlog/css', express.static(__dirname + '/public/css'));
+app.use('/viewBlog/js', express.static(__dirname + '/public/js'));
 
 app.set('view engine','ejs');
 
@@ -25,6 +27,17 @@ app.get("/about", function(req,res){
     res.render("about")
 });
 
+app.get("/viewBlog/:i",function(req,res){
+    const arrayValue = req.params.i;
+    console.log(data)
+    if (data[arrayValue]) {
+        res.render('viewBlog', { titlePost: data[arrayValue].title, blogPost: data[arrayValue].blog });
+      } else {
+        res.status(404).send('Blog post not found');
+      }
+    //res.render('viewBlog', {titlePost:data[arrayValue].title,blogPost:data[arrayValue].blog})
+})
+
 app.post("/blog", function(req,res){
     const newBlog = {
         title: req.body.title,
@@ -33,6 +46,7 @@ app.post("/blog", function(req,res){
     data.push(newBlog)
     res.redirect("/")
 })
+
 
 app.listen(3000, function(){
     console.log("Server is running.");
