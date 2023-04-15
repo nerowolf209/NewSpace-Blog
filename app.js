0ec6425e376c792e3396b58c9f83ed5e05ab3916
James Cooper <james.ioioio55@gmail.com>
2023-04-14 19:41:53 -0600

    Created Home, About and Create pages. Updated the CSS to better display. Started working on the form on the create page

--- app.js
+++ app.js
@@ -4,16 +4,31 @@ const bodyParser = require("body-parser");
 const app = express();
 
 // this is commonly used when accessing css files. 
-//app.use(express.static('public'));
+app.use(express.static('public'));
+
+app.set('view engine','ejs');
 
 // This is required to use the bodyParser for HTML sites
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const blogData = []
+let data = {"title":"","blog":""};
 
 app.get("/", function(req,res){
     res.render("home")
 });
 
+app.get("/blog", function(req,res){
+    res.render("createBlog")
+});
+
+app.get("/about", function(req,res){
+    res.render("about")
+});
+
+app.post("/blog", function(req,res){
+    console.log(req.body)
+})
 
 app.listen(3000, function(){
     console.log("Server is running.");
